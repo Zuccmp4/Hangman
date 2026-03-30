6b4438af26227b6b4545f79b17ff541a45f78354
Zuccmp4 <aeira49@gmail.com>
2026-03-30 10:50:34 -0400

    added guessLetter function and added more to the other functions to get it to work

--- hangman.js
+++ hangman.js
@@ -1,21 +1,44 @@
 const POSSIBLE_WORDS = ["obdurate", "defenestrate",
      "nocturnal", "verminous", "valor", "dahlia", "frog",
-      "toad", "albert", "gerbert", "salayka", "kitty"];
+      "toad", "albert", "gerbert", "salayka", "kitty", 
+      "Meshuggah", "Metal", "Thrash", "Death", "Frederick"];
+
+var word = "";
+var guesses = "";
 
 let newGame = function(){
     //Pick a random word
     let randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
-    let word = POSSIBLE_WORDS[randomIndex]; 
-    console.log("Word Chosen: ", word);
+    word = POSSIBLE_WORDS[randomIndex];
+    guesses = "";
 
     //show the dashes
+    updatePage();
+}
+
+let updatePage = function(){
     let clueString = "";
     for (let i = 0; i< word.length; i++)
     {
-        clueString += "_ ";
+        var currentLetter = word.charAt(i);
+        if(guesses.indexOf(currentLetter)>=0){
+            clueString+=currentLetter+" ";
+        }
+        else {
+            clueString += "_ ";
+        }
     }
-
     let clue = document.getElementById("clue");
     clue.textContent = clueString;
+
+    let guessArea = document.getElementById("guesses");
+    guessArea.textContent = "Guesses: "+ guesses + " ";
+}
+
+let guessLetter = function(){
+    let input = document.getElementById("guess");
+    let letter = input.value;
+    guesses+=letter;
+    updatePage();
 }
 
